381c79eabee55ff646fdf47c4c097a67a5e7a6d6
Leon Hollis <leonmbhollis@gmail.com>
2026-07-02 15:47:10 -0700

    added a "home page"

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require('cors');
+const path = require('path');
 
 // Define a route for GET requests to the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+  res.sendFile(path.join(__dirname, '..', 'frontend/HomePage.html'));
 });
 
 
-const path = require('path');
 app.use(express.static(path.join('..','frontend'))); //applys the css and javascript to the html
 
 app.get('/form', (req, res) => {
- res.sendFile(path.join(__dirname, '..', 'frontend/RoboticsScoutingForm.html')); //opens the html at /form
+ res.sendFile(path.join(__dirname, '..', 'frontend/RoboticsScoutingForm.html')); //opens the html at /fr
 });
  
 // Start the server
